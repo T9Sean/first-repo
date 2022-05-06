f38d2e645dc553bc82f1a1821b2b3e43bea77353
T9Sean <tulgoo1031@gmail.com>
2022-05-06 13:21:03 +0900

    year dream
    yeardream

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
 print("hello")
 
 
-for i in range(1,11):
-    if i%3 == 0:
+for i in range(1,21):
+    if i%3 == 0 and i != 15:
         print('year')
-    else:
-        print(i)
+    elif i%5 == 0, i != 15:
+        print('drean')
+    elif i == 5:
+        print('yeardream')
+
+
 
